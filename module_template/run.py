96bc4f35d833b4bb5971ed672b3474ea4d9b93ac
gabrielfior <fior.gbm@gmail.com>
2025-01-10 12:38:25 -0300

    Some more types and questions

--- module_template/run.py
+++ module_template/run.py
@@ -4,6 +4,8 @@ from typing import Dict
 from naptha_sdk.schemas import AgentRunInput
 from naptha_sdk.user import sign_consumer_id
 from naptha_sdk.utils import get_logger
+from web3.types import TxParams
+
 from module_template.schemas import InputSchema
 
 load_dotenv()
@@ -11,19 +13,21 @@ load_dotenv()
 logger = get_logger(__name__)
 
 # You can create your module as a class or function
-class BasicModule:
+class OmenBuyTransactionModule:
+    """ Returns TxParams for buying outcome tokens from a given Omen market on Gnosis chain. """
     def __init__(self, module_run):
         self.module_run = module_run
 
     def func(self, input_data):
         logger.info(f"Running module function")
-        return input_data
+        tx_params: TxParams = {}
+        return tx_params
 
 # Default entrypoint when the module is executed
 def run(module_run: Dict):
     module_run = AgentRunInput(**module_run)
-    module_run.inputs = InputSchema(**module_run.inputs)
-    basic_module = BasicModule(module_run)
+    module_run.inputs = InputSchema(**module_run.inputs.model_dump())
+    basic_module = OmenBuyTransactionModule(module_run)
     method = getattr(basic_module, module_run.inputs.func_name, None)
     return method(module_run.inputs.func_input_data)
 
@@ -37,10 +41,13 @@ if __name__ == "__main__":
 
     deployment = asyncio.run(setup_module_deployment("agent", "module_template/configs/deployment.json", node_url = os.getenv("NODE_URL")))
 
-    input_params = {
-        "func_name": "func",
-        "func_input_data": "gm...",
-    }
+    input_params = InputSchema(market_id= "0x123",
+                               from_address="0x456",
+                               collateral_wei_amount=1000000000000000000,
+                               func_name= "func",
+                func_input_data='gm...',
+    )
+
 
     module_run = {
         "inputs": input_params,
--- module_template/schemas.py
+++ module_template/schemas.py
@@ -1,6 +1,15 @@
+from typing import Optional, Union, Dict, Any, List
+
+from hexbytes import HexBytes
 from pydantic import BaseModel
-from typing import Union, Dict, Any, List, Optional
+from web3.types import Wei
+
 
 class InputSchema(BaseModel):
+    # ToDo - Use HexBytes and Wei once PMAT available.
+    market_id: str
+    from_address: str
+    collateral_wei_amount: int
+    # ToDo - Why do I define func_name and func_input_data here?
     func_name: str
     func_input_data: Optional[Union[Dict[str, Any], List[Dict[str, Any]], str]] = None
